81900e33cce38c4d9b0b6bfd42ed115d6000bbe6
nikilr2 <nikilr2@illinois.edu>
2020-03-18 22:54:18 -0500

    Added ktor as a dependency and added a block of code to run a local host server

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    val ktorVersion = "1.3.0"
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.0")
     testImplementation("org.slf4j:slf4j-simple:1.7.26")
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
 fun hello(): String {
     return "Hello, world! "
+}
+
+fun main() {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, World, KotlinNiki!")
+            }
+        }
+    }.start(wait = true)
 }
